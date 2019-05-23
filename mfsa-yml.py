de3423142027fcc222f4f1c27df6ac7daf938062
Martin Sirringhaus <martin.sirringhaus@suse.com>
2019-05-23 08:38:48 +0200

    mfsa-yml: Port to python3. Now needs python3-PyYAML installed!

--- mfsa-yml.py
+++ mfsa-yml.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import yaml
 import string
 import sys
@@ -7,7 +7,7 @@ import textwrap
 
 
 if len(sys.argv) <= 1:
-    print "Usage: {0} <mfsa.yml> [bsc#]".format(sys.argv[0])
+    print("Usage: {0} <mfsa.yml> [bsc#]".format(sys.argv[0]))
     exit(1)
 
 if len(sys.argv) > 2:
@@ -17,11 +17,10 @@ else:
 
 try:
     inpf = open(sys.argv[1], 'r')
-
     mfsa_data = inpf.read()
     inpf.close()
 except Exception as e:
-    print e
+    print(e)
     exit(1)
 
 
@@ -39,7 +38,7 @@ wrapCHLOG = textwrap.TextWrapper(
         subsequent_indent = "    ",
         width = 65)
 
-print "- update to {0} (bsc#{1})".format(mfsa_yaml['fixed_in'][0], bsc)
+print("- update to {0} (bsc#{1})".format(mfsa_yaml['fixed_in'][0], bsc))
 for a in mfsa_yaml['advisories']:
     cve = a
     bmos = []
@@ -47,8 +46,8 @@ for a in mfsa_yaml['advisories']:
         bmos += str(b['url']).split(', ')
     bmos.sort()
     bmos = map(lambda b: "bmo#" + b, bmos)
-    print "  * " + mfsa + "/" + cve
-    print wrapBMO.fill("({0})".format(", ".join(bmos)))
-    print wrapCHLOG.fill(mfsa_yaml['advisories'][a]['title'])
+    print("  * " + mfsa + "/" + cve)
+    print(wrapBMO.fill("({0})".format(", ".join(bmos))))
+    print(wrapCHLOG.fill(mfsa_yaml['advisories'][a]['title']))
 
 
