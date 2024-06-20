545dee24134936c467933f3332a48b677ae1bf42
Martin Sirringhaus <martin.sirringhaus@suse.com>
2024-06-20 15:58:55 +0200

    create-tar.sh: Upstream switched to a monorepo for Firefox-l10n

--- create-tar.sh
+++ create-tar.sh
@@ -84,6 +84,7 @@ function set_internal_variables() {
   FTP_URL="https://ftp.mozilla.org/pub/$PRODUCT/releases/$VERSION$VERSION_SUFFIX/source"
   FTP_CANDIDATES_BASE_URL="https://ftp.mozilla.org/pub/%s/candidates"
   LOCALES_URL="https://product-details.mozilla.org/1.0/l10n"
+  FF_L10N_MONOREPO="https://github.com/mozilla-l10n/firefox-l10n"
   PRODUCT_URL="https://product-details.mozilla.org/1.0"
   ALREADY_EXTRACTED_LOCALES_FILE=0
 }
@@ -500,10 +501,6 @@ function clone_and_repackage_locales() {
     FF_L10N_BASE="l10n_ff"
   fi
 
-  test ! -d $FF_L10N_BASE && mkdir $FF_L10N_BASE
-  # No-op, if we are building FF:
-  test ! -d $FINAL_L10N_BASE && mkdir $FINAL_L10N_BASE
-
   # This is only relevant for Thunderbird-builds
   # Here, the relevant directories we need to copy from FF and from TB
   # are specified in a python-file in the tarball
@@ -512,31 +509,35 @@ function clone_and_repackage_locales() {
   tb_locale_template=$(get_locales_directories "COMM_STRINGS_PATTERNS")
 
   echo "Fetching Browser locales..."
-  jq -r 'to_entries[]| "\(.key) \(.value|.revision)"' "$FF_LOCALE_FILE" | \
-    while read -r locale changeset ; do
-      case $locale in
-        ja-JP-mac|en-US)
-          ;;
-        *)
-          echo "reading changeset information for $locale"
-          echo "fetching $locale changeset $changeset ..."
-          if [ -d "$FF_L10N_BASE/$locale/.hg" ]; then
-            pushd "$FF_L10N_BASE/$locale" || exit 1
-            hg pull || exit 1
-            popd || exit 1
-          else
-            hg clone "https://hg.mozilla.org/l10n-central/$locale" "$FF_L10N_BASE/$locale" || exit 1
-          fi
-          [ "$RELEASE_TAG" == "default" ] || hg -R "$FF_L10N_BASE/$locale" up -C -r "$changeset" || exit 1
+  if [ -d "$FF_L10N_BASE/.git" ]; then
+    pushd "$FF_L10N_BASE/" || exit 1
+    git pull || exit 1
+    popd || exit 1
+  else
+    git clone "$FF_L10N_MONOREPO" "$FF_L10N_BASE" || exit 1
+  fi
+  # Currently all locales show the same changeset-hash, as they moved to a monorepo. We just take the first one.
+  changeset=$(jq -r 'to_entries[0]| "\(.key) \(.value|.revision)"' "$FF_LOCALE_FILE" | cut -d " " -f 2)
+  [ "$RELEASE_TAG" == "default" ] || git -C "$FF_L10N_BASE/" switch --detach "$changeset" || exit 1
 
-          # If we are doing TB, we have to merge both l10n-repos
-          if [ "$PRODUCT" = "thunderbird" ] && test -d "$TB_L10N_BASE/$locale/" ; then
+  # No-op, if we are building FF:
+  test ! -d $FINAL_L10N_BASE && mkdir $FINAL_L10N_BASE
+
+  # If we are doing TB, we have to merge both l10n-repos
+  if [ "$PRODUCT" = "thunderbird" ] && test -d "$TB_L10N_BASE/$locale/" ; then
+    jq -r 'to_entries[]| "\(.key) \(.value|.revision)"' "$FF_LOCALE_FILE" | \
+      while read -r locale changeset ; do
+        case $locale in
+          ja-JP-mac|en-US)
+            ;;
+          *)
             create_and_copy_locales "$locale" "$FF_L10N_BASE" "$ff_locale_template" "$FINAL_L10N_BASE"
             create_and_copy_locales "$locale" "$TB_L10N_BASE" "$tb_locale_template" "$FINAL_L10N_BASE"
-          fi
-          ;;
-      esac
-    done
+            ;;
+        esac
+      done
+  fi
+
   echo "creating l10n archive..."
   local TAR_FLAGS="--exclude-vcs"
   if [ "$PRODUCT" = "thunderbird" ]; then
